9eb0ddbfb71068c227662eb81eec7881a8b32cdb
Pablo Flores <a20203651@pucp.edu.pe>
2023-06-16 17:28:16 -0500

    Base Datos

--- BaseDatosLab8.sql
+++ BaseDatosLab8.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `lab08` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `lab08`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: lab08
@@ -124,4 +126,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-16 17:04:56
+-- Dump completed on 2023-06-16 17:24:49
